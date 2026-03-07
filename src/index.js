ee5db37f679445a78ee40b918ef212dc24fd178d
DbRDYZmMRu <frithmangrove@gmail.com>
2026-03-07 20:03:32 +0000

    Changed the Base URL

--- src/index.js
+++ src/index.js
@@ -93,7 +93,7 @@ export default {
     
     // Generate canonical book slug/URL
     const bookCleanSlug = createSlug(book.bookTitle);
-    const bookUrl = `https://www.frithhilton.com.ng/published/poetry/${bookCleanSlug}`;
+    const bookUrl = `https://legacy.frithhilton.com/published/poetry/${bookCleanSlug}`;
     
     if (!poemSlugRaw) {
       // Serve Book Index Page
